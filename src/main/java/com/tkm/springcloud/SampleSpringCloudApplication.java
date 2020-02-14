ff48a685700ea0fafcae238a9b62c4142955b558
thilina <thilina.kalum@texussolutions.com>
2020-02-14 18:46:10 +0530

    initial commit

---
+++ src/main/java/com/tkm/springcloud/SampleSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.tkm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tkm/springcloud/SampleSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkm.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
